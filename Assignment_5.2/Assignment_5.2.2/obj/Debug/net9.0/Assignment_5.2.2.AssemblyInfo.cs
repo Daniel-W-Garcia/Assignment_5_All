14113d3df6c008bac8f60fb08e102cf4f64b8979
itodobien <daniel.w.garcia@outlook.com>
2025-05-06 11:27:04 -0700

    5.2.2
    recursion method to print ints from 1 - n. added screen grab

--- Assignment_5.2/Assignment_5.2.2/obj/Debug/net9.0/Assignment_5.2.2.AssemblyInfo.cs
+++ Assignment_5.2/Assignment_5.2.2/obj/Debug/net9.0/Assignment_5.2.2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment_5.2.2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b29f547c0d31a2219eeb28f0c18053ed17c8a2b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03baffa76f8757a621ef71e02e78b3f2ede9d24f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment_5.2.2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment_5.2.2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
