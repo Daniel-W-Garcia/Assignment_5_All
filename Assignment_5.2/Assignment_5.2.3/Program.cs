f9d83eb5bbbb95a29a5aaf05dba2eb4993004654
itodobien <daniel.w.garcia@outlook.com>
2025-05-06 11:35:38 -0700

    5.3.3
    Same as the last assignment  but printed in reverse using tail recursion

---
+++ Assignment_5.2/Assignment_5.2.3/Program.cs
@@ -0,0 +1,20 @@
+﻿// use recursion to print all natural numbers from  'n' to 1
+// method should just decrement and print n
+// calls itself until 1 - n is reached
+// func void Print (int) 
+//  {
+//      if n <= 0
+//      CW(n)
+//      n--
+//  }
+
+void PrintIntReversed(int n)
+{
+    if (n > 0)
+    {
+        Console.WriteLine(n);
+        PrintIntReversed(n - 1);
+    }
+}
+
+PrintIntReversed(5);
